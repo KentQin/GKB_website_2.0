35219b55e09abae349e1ed3a998f3cc35778d19b
KentQin <kentqin49@gmail.com>
2017-03-19 15:01:59 +1100

    set up hot realod for react.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+    devtool: 'eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js')
+    ],
+    output:{
+        path: '/',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.join(__dirname, 'client'),
+                    path.join(__dirname, 'server/shared')
+                ],
+                loaders: ['react-hot-loader', 'babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    }
+}
