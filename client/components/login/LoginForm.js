87ccdf5cfd4e6d377d4ea20b01de884a0052fdf6
KentQin <kentqin49@gmail.com>
2017-03-20 11:51:36 +1100

    Signup is allowed to send post request -- next set up redux

--- client/components/login/LoginForm.js
+++ client/components/login/LoginForm.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username_email: '',
+            password: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
     render() {
         return (
             <form className="form-horizontal">
                 <h1 className="h-e-a-d-e-r-t-e-x-t">LOGIN</h1>
                 <div className="form-group">
-                    <input type="text" className="form-control input-w-60" id="exampleInputEmail1" placeholder="Username/ Email" />
+                    <input
+                        value={this.state.username_email}
+                        onChange={this.onChange}
+                        name="username_email"
+                        type="text"
+                        className="form-control input-w-60"
+                        id="exampleInputEmail1"
+                        placeholder="Username/ Email" />
                 </div>
                 <div className="form-group">
-                    <input type="password" className="form-control input-w-60" id="exampleInputPassword1" placeholder="Password" />
+                    <input
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        name="password"
+                        type="password"
+                        className="form-control input-w-60"
+                        id="exampleInputPassword1"
+                        placeholder="Password" />
                 </div>
                 <div className="form-group">
                     <label>
--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router';
+import axios from 'axios';
 
 class SignupForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            confrimPassword: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        /*axios.post(URL,{jason data})*/
+        axios.post('api/users', { user: this.state });
+    }
+
     render() {
         return (
-            <form className="form-horizontal">
+            <form
+                onSubmit={this.onSubmit}
+                className="form-horizontal"
+            >
                 <h1 className="h-e-a-d-e-r-t-e-x-t">SIGN UP</h1>
                 <div className="form-group">
-                    <input type="text" className="form-control input-w-60" id="exampleInputEmail1" placeholder="Email Adress" />
+                    <input
+                        value={this.state.email}
+                        onChange={this.onChange}
+                        name="email"
+                        type="text"
+                        className="form-control input-w-60"
+                        id="exampleInputEmail1"
+                        placeholder="Email Adress" />
                 </div>
                 <div className="form-group">
-                    <input type="password" className="form-control input-w-60" id="exampleInputPassword1" placeholder="Password" />
+                    <input
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        name="password"
+                        type="password"
+                        className="form-control input-w-60"
+                        id="exampleInputPassword1"
+                        placeholder="Password" />
                 </div>
                 <div className="form-group">
-                    <input type="password" className="form-control input-w-60" id="exampleInputConfirm" placeholder="Confirm Password" />
+                    <input
+                        value={this.state.confrimPassword}
+                        onChange={this.onChange}
+                        name="confrimPassword"
+                        type="password"
+                        className="form-control input-w-60"
+                        id="exampleInputConfirm"
+                        placeholder="Confirm Password" />
                 </div>
                 <button type="submit" className="btn btn-default">Submit</button>
                 <div className="form-group">
