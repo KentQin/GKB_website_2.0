623d7866f5cb664a8dc26fa11be3ce41d87b2870
KentQin <kentqin49@gmail.com>
2017-03-21 19:08:58 +1100

    server-side validation break; next pull

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config';
 
+import users from './routes/users';
+
 let app = express();
+
+// parse data from request body ==> use bodyParser middleware
+app.use(bodyParser.json());
+
+// match url, then apply the middleware
+app.use('/api/users', users);
 
 const compiler = webpack(webpackConfig);
 // set up hot reload for reacjs
@@ -22,6 +31,7 @@ app.use(express.static('public'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
+
 
 console.log(app.listen(9000, () => console.log('Running on localhost:9000')));
