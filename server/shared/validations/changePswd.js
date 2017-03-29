c2a41a9f395ef3754fcebb021c4bc9bcb9408818
Prajith Manian <Prajith Manian>
2017-03-29 13:18:08 +1100

    Dummy changepswd functionality

---
+++ server/shared/validations/changePswd.js
@@ -0,0 +1,30 @@
+import lodash from 'lodash';
+import validator from 'validator';
+
+export default function validateInput(data) {
+    let errors = {};
+
+    // if password is empty, push error
+    if (data.password.length < 8) {
+        errors.password = 'Password needs to be at least 8 characters long';
+    }
+    // if password is empty, push error
+    if (validator.isEmpty(data.password)) {
+        errors.password = 'Password is required';
+    }
+    if (!validator.equals(data.password,data.confirmPassword)) {
+        errors.confirmPassword = 'Password and Confirm Password do not match';
+    }
+    // if confirmPassword is empty, push error
+    if (validator.isEmpty(data.confirmPassword)) {
+        errors.confirmPassword = 'Please confirm your password';
+    }
+
+    console.log("errors: ", errors);
+
+    return {
+        errors,
+        isValid: lodash.isEmpty(errors)
+    }
+
+}
