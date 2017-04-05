7b1631c9b0d077edc6320ccf1698ce0e3765cfc8
KentQin <kentqin49@gmail.com>
2017-04-05 13:59:38 +1000

    pull-1

--- client/actions/resetpswdAction.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export function resetpwdRequest(userData) {
-    return dispatch => {
-        return axios.post('/api/resetpwd', userData);
-    }
-}
--- server/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  dev: true,
-}
-
-export default config;
