10b3735edcffd106483988b96336104f368ea802
ZHANG RUOQ <rzha9657@uni.sydney.edu.au>
2017-04-26 11:42:44 +1000

    Landing Page

--- client/components/landing/landingPage.js
+++ client/components/landing/landingPage.js
@@ -6,14 +6,9 @@ import {Link} from 'react-router';
 class landingPage extends React.Component{
     render(){
         return(
-            // {/*<div className="container landingPage">*/}
-            //     {/*<h1>Locate your destination in one sentence</h1>*/}
-            //     {/*<SearchBar searchBarRequest={this.props.searchBarRequest}/>*/}
-            //     {/*<Link to="" />Learn more*/}
-            // {/*</div>*/}
         <div className="jumbotron">
-            <div className="container">
-                <h3>Locate your destination in one sentence</h3>
+            <div className="container landingPage">
+                <h3 className="title-white">Locate your destination in one sentence</h3>
                 <SearchBar searchBarRequest={this.props.searchBarRequest}/>
                 <p><a className="btn" href="#" role="button">Learn more</a></p>
             </div>
