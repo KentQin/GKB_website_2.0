e24830b080ad9a89d37af27a4fc8eaf85d3f7cb2
ZHANG RUOQ <rzha9657@uni.sydney.edu.au>
2017-06-06 01:23:16 +1000

    to pull

--- server/database.js
+++ server/database.js
@@ -6,31 +6,31 @@ var config = {
     username:"ubuntu",
     host:"115.146.90.170",
     agent : process.env.SSH_AUTH_SOCK,
-    privateKey:require('fs').readFileSync('/Users/kenty/.ssh/gkb'),
+    privateKey:require('fs').readFileSync('/Users/zhangruoq/pb'),
     port:22,
     dstPort:27017,
 };
 
-// var server = tunnel(config, function (error, server) {
-//     if(error){
-//         console.log("SSH connection error: " + error);
-//     }
-//     mongoose.connect('mongodb://localhost/mydb', function(err) {
-//         if (err) {
-//             console.log(err);
-//             console.log("database not connected");
-//         } else {
-//             console.log("database connected to great");
-//         }
-//     });
-// });
-
-
-mongoose.connect('mongodb://localhost/mydb', function(err) {
-    if (err) {
-        console.log(err);
-        console.log("database not conencted");
-    } else {
-        console.log("database connected to great");
+var server = tunnel(config, function (error, server) {
+    if(error){
+        console.log("SSH connection error: " + error);
     }
+    mongoose.connect('mongodb://localhost/mydb', function(err) {
+        if (err) {
+            console.log(err);
+            console.log("database not connected");
+        } else {
+            console.log("database connected to great");
+        }
+    });
 });
+
+//
+// mongoose.connect('mongodb://localhost/mydb', function(err) {
+//     if (err) {
+//         console.log(err);
+//         console.log("database not conencted");
+//     } else {
+//         console.log("database connected to great");
+//     }
+// });
